bf6af0647c6d03c5ee0cd1d151f73201a9aeaa5c
Abigail Liang <ningchenliang98@gmail.com>
2023-11-05 01:45:09 +0000

    Initial commit

---
+++ constants/constants.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://10.138.0.2:9200"
+    ES_USERNAME = "Abi"
+    ES_PASSWORD = "12345Jxtsz"
+    STRIPE_API_KEY = "sk_test_51O49eyI79DUKaBtiWVOik5qeHceEvcdiWcTkGs7uhiV8BjGtAsSoGnxlLl1y8tI0KvpgteZVOH9OfBJCUV7fpnHu00cZAweIUt"
+    
+    GCS_BUCKET = "appstore_bucket_abi2"
+)
